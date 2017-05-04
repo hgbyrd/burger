5df4a7606955e8bbfae8950bf1a0062f8a6994a8
Guillermo Lopez <thatgee@calvisitor-10-105-145-188.calvisitor.1918.berkeley.edu>
2017-05-03 18:21:39 -0700

    Intermediate commit

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//not understanding of middleware
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
